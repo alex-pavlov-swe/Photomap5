3d598b13d9628a25f722d7fc00e80623d5f9e82b
PavlovAS <pavlov.cmc@gmail.com>
2020-05-24 14:38:15 -0700

    React added and functional

--- photomap5/photomap5/urls.py
+++ photomap5/photomap5/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('photo/', include('photo.urls'))
+    path('', include('frontend.urls')),
+    path('photo/', include('photo.urls')),
 ]
